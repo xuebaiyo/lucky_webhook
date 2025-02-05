4a4f4564541a7ceba692a251ac9b1e2b51aa2e9a
xuebai <53637517+xuebaiyo@users.noreply.github.com>
2025-02-05 12:33:54 +0800

    Add files via upload

--- webhookReceiver.php
+++ webhookReceiver.php
@@ -1,9 +1,8 @@
 <?php
 
 require 'project/config.php';
-//设置opentoken
-//$targetToken='0SHRcH7m3OBCyhNXylt08gIP4kdeUnJ8'; //输入你的token,与lucky中opentoken一致
-
+// 设置 opentoken
+// $targetToken='0SHRcH7m3OBCyhNXylt08gIP4kdeUnJ8'; // 输入你的 token,与 lucky 中 opentoken 一致
 
 // 存储内容的文本文件路径
 $filePath = 'project/webhook_contents.txt';
@@ -27,6 +26,29 @@ if ($found) {
 
     // 确保追加内容在新行
     file_put_contents($filePath, PHP_EOL. $data. '|'. $currentTime. PHP_EOL, FILE_APPEND);
+
+    // 执行 Bash 脚本
+    $scriptPath = 'project/autostart.sh';
+    if (file_exists($scriptPath) && is_executable($scriptPath)) {
+        // 使用 exec 函数执行脚本
+        exec($scriptPath, $output, $returnCode);
+        if ($returnCode === 0) {
+            // 脚本执行成功
+            //echo "脚本执行成功";
+            // 返回成功响应
+            http_response_code(200);
+            echo 'update_done';
+        } else {
+            // 脚本执行失败
+            echo "脚本执行失败，返回码: $returnCode";
+            http_response_code(401);
+            echo 'update_fail:'.$returnCode;
+        }
+    } else {
+        echo "脚本文件不存在或不可执行";
+        http_response_code(401);
+        echo 'update_fail:脚本不存在或者不可执行';
+    }
 
     // 返回成功响应
     http_response_code(200);
